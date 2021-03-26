7110260cba4a05530ab8272d07ac1af772ed302f
Yannick Sagaert <yannick.sagaert@student.howest.be>
2021-03-26 09:22:44 +0100

    Update DarStatusResult

--- DjustConnect.PartnerAPI.Client/DjustConnect.PartnerAPI.Client/DarStatusResult.cs
+++ DjustConnect.PartnerAPI.Client/DjustConnect.PartnerAPI.Client/DarStatusResult.cs
@@ -24,11 +24,11 @@ namespace DjustConnect.PartnerAPI.Client
 
         public string ToJson()
         {
-            return Newtonsoft.Json.JsonConvert.SerializeObject(this);
+            return JsonConvert.SerializeObject(this);
         }
         public static DarStatusResult FromJson(string data)
         {
-            return Newtonsoft.Json.JsonConvert.DeserializeObject<DarStatusResult>(data);
+            return JsonConvert.DeserializeObject<DarStatusResult>(data);
         }
     }
 }
