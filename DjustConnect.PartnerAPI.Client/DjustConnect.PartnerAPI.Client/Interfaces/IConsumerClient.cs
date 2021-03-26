ec2ffa9e21b33b6fab2baf7930c74511cab0d043
Yannick Sagaert <yannick.sagaert@student.howest.be>
2021-03-26 09:14:45 +0100

    Add IConsumerClient interface

---
+++ DjustConnect.PartnerAPI.Client/DjustConnect.PartnerAPI.Client/Interfaces/IConsumerClient.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DjustConnect.PartnerAPI.Client.Interfaces
+{
+    public interface IConsumerClient
+    {
+        //System.Threading.Tasks.Task<DarStatusResult[]> GetDarStatusAsync(string farmNumberFilter);
+        //System.Threading.Tasks.Task<DarStatusResult[]> GetDarStatusAsync(string farmNumberFilter, System.Threading.CancellationToken cancellationToken);
+        //System.Threading.Tasks.Task<FarmStatusResult[]> GetFarmStatusAsync(string farmNumberFilter);
+        //System.Threading.Tasks.Task<FarmStatusResult[]> GetFarmStatusAsync(string farmNumberFilter, System.Threading.CancellationToken cancellationToken);
+    }
+}
