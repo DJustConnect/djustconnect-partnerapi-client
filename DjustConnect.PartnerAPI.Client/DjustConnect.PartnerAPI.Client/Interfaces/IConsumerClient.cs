16c711abcf6b7bfec48758c956575c081944a405
Yannick Sagaert <yannick.sagaert@student.howest.be>
2021-03-26 09:36:04 +0100

    Update IConsumerClient interface

--- DjustConnect.PartnerAPI.Client/DjustConnect.PartnerAPI.Client/Interfaces/IConsumerClient.cs
+++ DjustConnect.PartnerAPI.Client/DjustConnect.PartnerAPI.Client/Interfaces/IConsumerClient.cs
@@ -3,14 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace DjustConnect.PartnerAPI.Client.Interfaces
 {
     public interface IConsumerClient
     {
-        //System.Threading.Tasks.Task<DarStatusResult[]> GetDarStatusAsync(string farmNumberFilter);
-        //System.Threading.Tasks.Task<DarStatusResult[]> GetDarStatusAsync(string farmNumberFilter, System.Threading.CancellationToken cancellationToken);
-        //System.Threading.Tasks.Task<FarmStatusResult[]> GetFarmStatusAsync(string farmNumberFilter);
-        //System.Threading.Tasks.Task<FarmStatusResult[]> GetFarmStatusAsync(string farmNumberFilter, System.Threading.CancellationToken cancellationToken);
+        Task<DarStatusResult[]> GetDarStatusAsync(string farmNumberFilter);
+        Task<DarStatusResult[]> GetDarStatusAsync(string farmNumberFilter, CancellationToken cancellationToken);
+        Task<FarmStatusResult[]> GetFarmStatusAsync(string farmNumberFilter);
+        Task<FarmStatusResult[]> GetFarmStatusAsync(string farmNumberFilter, CancellationToken cancellationToken);
     }
 }
