3e9565158ae75c7e38fcf00ecec7058a7e83fb3d
Yannick Sagaert <yannick.sagaert@student.howest.be>
2021-03-26 09:42:28 +0100

    Update DjustConnectClient

--- DjustConnect.PartnerAPI.Client/DjustConnect.PartnerAPI.Client/DjustConnectClient.cs
+++ DjustConnect.PartnerAPI.Client/DjustConnect.PartnerAPI.Client/DjustConnectClient.cs
@@ -10,31 +10,31 @@ namespace DjustConnect.PartnerAPI.Client
 {
     public class DjustConnectClient
     {
-        // TODO How to handle the ACC base url?
-  //      public string BaseUrl { get; set; } = "https://partnerapi.djustconnect.be/";
-  //      protected System.Net.Http.HttpClient _httpClient;
-  //      public DjustConnectClient(System.Net.Http.HttpClient httpClient) : base(httpClient)
-  //      {
-  //          _httpClient = httpClient;
-  //      }
+       //TODO How to handle the ACC base url?
+        //public string BaseUrl { get; set; } = "https://partnerapi.djustconnect.be/";
+        //protected System.Net.Http.HttpClient _httpClient;
+        //public DjustConnectClient(HttpClient httpClient) : base(httpClient)
+        //{
+        //    _httpClient = httpClient;
+        //}
 
-		//public static System.Net.Http.HttpClient CreateHttpClient(string thumbprint, string subscriptionkey)
-		//{
-		//	var store = new X509Store("My", StoreLocation.LocalMachine);
-		//	store.Open(OpenFlags.ReadOnly);
-		//	var certificates = store.Certificates.Find(X509FindType.FindByThumbprint, session.CertThumbprint, false);
-		//	if (certificates.Count == 0)
-		//	{
-		//		throw new InvalidOperationException($"Certificate not found for CN=client.ikmnet.be in LocalMachine/My.");
-		//	}
-		//	var certificate = certificates[0];
-		//	//var certificate = new X509Certificate2(session.CertificatePath, session.CertificatePassword, X509KeyStorageFlags.MachineKeySet);
-		//	var clientHandler = new HttpClientHandler();
-		//	clientHandler.ClientCertificates.Add(certificate);
-		//	clientHandler.ClientCertificateOptions = ClientCertificateOption.Manual;
-		//	http = new HttpClient(clientHandler);
-		//	http.DefaultRequestHeaders.Add("DjustConnect-Subscription-Key", subscriptionkey);
-		//	return http;
-		//}
-	}
+        //public static System.Net.Http.HttpClient CreateHttpClient(string thumbprint, string subscriptionkey)
+        //{
+        //    var store = new X509Store("My", StoreLocation.LocalMachine);
+        //    store.Open(OpenFlags.ReadOnly);
+        //    var certificates = store.Certificates.Find(X509FindType.FindByThumbprint, session.CertThumbprint, false);
+        //    if (certificates.Count == 0)
+        //    {
+        //        throw new InvalidOperationException($"Certificate not found for CN=client.ikmnet.be in LocalMachine/My."); // Change to correct CN
+        //    }
+        //    var certificate = certificates[0];
+        //    //var certificate = new X509Certificate2(session.CertificatePath, session.CertificatePassword, X509KeyStorageFlags.MachineKeySet);
+        //    var clientHandler = new HttpClientHandler();
+        //    clientHandler.ClientCertificates.Add(certificate);
+        //    clientHandler.ClientCertificateOptions = ClientCertificateOption.Manual;
+        //    http = new HttpClient(clientHandler);
+        //    http.DefaultRequestHeaders.Add("DjustConnect-Subscription-Key", subscriptionkey);
+        //    return http;
+        //}
+    }
 }
