e01feeab9e7dc587b12cd8e70659c6c2dd8ea2a0
Yannick Sagaert <yannick.sagaert@student.howest.be>
2021-03-26 09:25:44 +0100

    Add DjustConnectException

---
+++ DjustConnect.PartnerAPI.Client/DjustConnect.PartnerAPI.Client/DjustConnectException.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DjustConnect.PartnerAPI.Client
+{
+    public partial class DjustConnectException : System.Exception
+    {
+        public int StatusCode { get; private set; }
+        public string Response { get; private set; }
+
+        public Dictionary<string, IEnumerable<string>> Headers { get; private set; }
+
+        //public SwaggerException(string message, int statusCode, string response, Dictionary<string, IEnumerable<string>> headers, Exception innerException)
+        //    : base(message + "\n\nStatus: " + statusCode + "\nResponse: \n" + response.Substring(0, response.Length >= 512 ? 512 : response.Length), innerException)
+        //{
+        //    StatusCode = statusCode;
+        //    Response = response;
+        //    Headers = headers;
+        //}
+
+        public override string ToString()
+        {
+            return string.Format("HTTP Response: \n\n{0}\n\n{1}", Response, base.ToString());
+        }
+    }
+}
