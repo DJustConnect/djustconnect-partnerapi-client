8f93a1959c0f3c034ddf5408d4feafea0b0137f8
Yannick Sagaert <yannick.sagaert@student.howest.be>
2021-03-26 09:52:49 +0100

    Update ConsumerClient

--- DjustConnect.PartnerAPI.Client/DjustConnect.PartnerAPI.Client/ConsumerClient.cs
+++ DjustConnect.PartnerAPI.Client/DjustConnect.PartnerAPI.Client/ConsumerClient.cs
@@ -3,10 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net.Http;
+using DjustConnect.PartnerAPI.Client.Interfaces;
 
 namespace DjustConnect.PartnerAPI.Client
 {
-    public class ConsumerClient
+    public class ConsumerClient : DjustConnectClient
     {
-    }
+        //public ConsumerClient(HttpClient httpClient) : base(httpClient)
+        //{
+        //}
+
+		/* TODO first next step(s):
+			api/Resource
+			api/FarmIdType
+			api/FarmMapping
+			api/Consumer/resource-health
+			
+			api/RarStatus
+			api/ConsumerAccess - GET
+			api/ConsumerAccess - POST
+			
+			api/Consumer/push - GET
+			api/Consumer/push/activate - POST
+			api/Consumer/push/deactivate - POST
+		*/
+
+
+	}
 }
