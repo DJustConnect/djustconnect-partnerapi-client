9fab6e15e1df4cc8c15f5dce66dc7f41d5cbc8db
Yannick Sagaert <yannick.sagaert@student.howest.be>
2021-03-26 09:46:11 +0100

    Update ProviderClient

---
+++ DjustConnect.PartnerAPI.Client/DjustConnect.PartnerAPI.Client/ProviderClient.cs
@@ -0,0 +1,17 @@
+﻿using DjustConnect.PartnerAPI.Client.Interfaces;
+using System.Net.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DjustConnect.PartnerAPI.Client
+{
+    public class ProviderClient : DjustConnectClient, IProviderClient
+    {
+        //public ProviderClient(HttpClient httpClient) : base(httpClient)
+        //{
+        //}
+    }
+}
