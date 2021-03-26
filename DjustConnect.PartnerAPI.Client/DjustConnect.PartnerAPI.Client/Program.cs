681d3cdf80ef60f7bef198e7c954dd82b51b0fad
Yannick Sagaert <yannick.sagaert@student.howest.be>
2021-03-26 11:47:45 +0100

    Add comments to program class

--- DjustConnect.PartnerAPI.Client/DjustConnect.PartnerAPI.Client/Program.cs
+++ DjustConnect.PartnerAPI.Client/DjustConnect.PartnerAPI.Client/Program.cs
@@ -9,12 +9,12 @@ namespace DjustConnect.PartnerAPI.Client
 {
     public class Program
     {
-        public void Main() // How to use this http var?
+        public void Main() // Move this logic to the test project - Arrange?
 		{
             var http = DjustConnectClient.CreateHttpClient("thumbprint here", "subscription key here");
             var consumerclient = new ConsumerClient(http);
             var darstatusresults = consumerclient.GetDarStatusAsync("farm number here").Result;
-            // TODO do something with dar status result
+            // TODO do something with dar status result - Assert to see if you get the correct status result?
         }
 	}
 }
