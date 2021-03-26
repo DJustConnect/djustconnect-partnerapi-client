c7656dcc9ed5390371bc9edeeb1d329f04832042
Yannick Sagaert <yannick.sagaert@student.howest.be>
2021-03-26 10:04:19 +0100

    Simplify ConsumerClient class

--- DjustConnect.PartnerAPI.Client/DjustConnect.PartnerAPI.Client/ConsumerClient.cs
+++ DjustConnect.PartnerAPI.Client/DjustConnect.PartnerAPI.Client/ConsumerClient.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using DjustConnect.PartnerAPI.Client.Interfaces;
 
 namespace DjustConnect.PartnerAPI.Client
@@ -14,7 +16,7 @@ namespace DjustConnect.PartnerAPI.Client
         //{
         //}
 
-		/* TODO first next step(s):
+        /* TODO first next step(s):
 			api/Resource
 			api/FarmIdType
 			api/FarmMapping
@@ -29,6 +31,187 @@ namespace DjustConnect.PartnerAPI.Client
 			api/Consumer/push/deactivate - POST
 		*/
 
+        /// <exception cref="SwaggerException">A server side error occurred.</exception> // DjustConnectException?
+        /// <exception cref="SwaggerException">A server side error occurred.</exception> // DjustConnectException?
+        //public Task<DarStatusResult[]> GetDarStatusAsync(string farmNumberFilter)
+        //{
+        //    return GetDarStatusAsync(farmNumberFilter, CancellationToken.None);
+        //}
 
-	}
+        /// <exception cref="SwaggerException">A server side error occurred.</exception> // DjustConnectException?
+        /// <param name="cancellationToken">A cancellation token that can be used by other objects or threads to receive notice of cancellation.</param>
+        //public async Task<DarStatusResult[]> GetDarStatusAsync(string farmNumberFilter, CancellationToken cancellationToken)
+        //{
+        //    var urlBuilder_ = new StringBuilder();
+        //    urlBuilder_.Append(BaseUrl != null ? BaseUrl.TrimEnd('/') : "").Append("/api/DarStatus?PageSize=100&");
+        //    urlBuilder_.Append("FarmNumberFilter=").Append(Uri.EscapeDataString(farmNumberFilter != null ? ConvertToString(farmNumberFilter, System.Globalization.CultureInfo.InvariantCulture) : "")).Append("&");
+        //    urlBuilder_.Length--;
+
+        //    var client_ = _httpClient;
+        //    try
+        //    {
+        //        using (var request_ = new HttpRequestMessage())
+        //        {
+        //            request_.Method = new HttpMethod("GET");
+        //            request_.Headers.Accept.Add(MediaTypeWithQualityHeaderValue.Parse("application/json"));
+
+        //            PrepareRequest(client_, request_, urlBuilder_);
+        //            var url_ = urlBuilder_.ToString();
+        //            request_.RequestUri = new Uri(url_, UriKind.RelativeOrAbsolute);
+        //            PrepareRequest(client_, request_, url_);
+
+        //            var response_ = await client_.SendAsync(request_, HttpCompletionOption.ResponseHeadersRead, cancellationToken).ConfigureAwait(false);
+        //            try
+        //            {
+        //                var headers_ = System.Linq.Enumerable.ToDictionary(response_.Headers, h_ => h_.Key, h_ => h_.Value);
+        //                if (response_.Content != null && response_.Content.Headers != null)
+        //                {
+        //                    foreach (var item_ in response_.Content.Headers)
+        //                        headers_[item_.Key] = item_.Value;
+        //                }
+
+        //                ProcessResponse(client_, response_);
+
+        //                var status_ = ((int)response_.StatusCode).ToString();
+        //                if (status_ == "200")
+        //                {
+        //                    var responseData_ = response_.Content == null ? null : await response_.Content.ReadAsStringAsync().ConfigureAwait(false);
+        //                    try
+        //                    {
+        //                        var result_ = Newtonsoft.Json.JsonConvert.DeserializeObject<DarStatusResult[]>(responseData_);
+        //                        return result_;
+        //                    }
+        //                    catch (System.Exception exception_)
+        //                    {
+        //                        throw new DjustConnectException("Could not deserialize the response body.", (int)response_.StatusCode, responseData_, headers_, exception_);
+        //                    }
+        //                }
+        //                else if (status_ != "200" && status_ != "204")
+        //                {
+        //                    var responseData_ = response_.Content == null ? null : await response_.Content.ReadAsStringAsync().ConfigureAwait(false);
+        //                    throw new DjustConnectException("The HTTP status code of the response was not expected (" + (int)response_.StatusCode + ").", (int)response_.StatusCode, responseData_, headers_, null);
+        //                }
+        //                return null;
+        //            }
+        //            finally
+        //            {
+        //                if (response_ != null)
+        //                    response_.Dispose();
+        //            }
+        //        }
+        //    }
+        //    finally
+        //    {
+        //    }
+        //}
+
+        ///// <exception cref="SwaggerException">A server side error occurred.</exception> // DjustConnectException?
+        //public Task<FarmStatusResult[]> GetFarmStatusAsync(string farmNumberFilter)
+        //{
+        //    return GetFarmStatusAsync(farmNumberFilter, CancellationToken.None);
+        //}
+
+        /// <exception cref="SwaggerException">A server side error occurred.</exception> // DjustConnectException?
+        /// <param name="cancellationToken">A cancellation token that can be used by other objects or threads to receive notice of cancellation.</param>
+        //public async Task<FarmStatusResult[]> GetFarmStatusAsync(string farmNumberFilter, CancellationToken cancellationToken)
+        //{
+        //    var urlBuilder_ = new System.Text.StringBuilder();
+        //    urlBuilder_.Append(BaseUrl != null ? BaseUrl.TrimEnd('/') : "").Append("/api/FarmStatus?");
+        //    urlBuilder_.Append("FarmNumberFilter=").Append(System.Uri.EscapeDataString(farmNumberFilter != null ? ConvertToString(farmNumberFilter, System.Globalization.CultureInfo.InvariantCulture) : "")).Append("&");
+        //    urlBuilder_.Length--;
+
+        //    var client_ = _httpClient;
+        //    try
+        //    {
+        //        using (var request_ = new HttpRequestMessage())
+        //        {
+        //            request_.Method = new HttpMethod("GET");
+        //            request_.Headers.Accept.Add(MediaTypeWithQualityHeaderValue.Parse("application/json"));
+
+        //            PrepareRequest(client_, request_, urlBuilder_);
+        //            var url_ = urlBuilder_.ToString();
+        //            request_.RequestUri = new Uri(url_, UriKind.RelativeOrAbsolute);
+        //            PrepareRequest(client_, request_, url_);
+
+        //            var response_ = await client_.SendAsync(request_, HttpCompletionOption.ResponseHeadersRead, cancellationToken).ConfigureAwait(false);
+        //            try
+        //            {
+        //                var headers_ = Enumerable.ToDictionary(response_.Headers, h_ => h_.Key, h_ => h_.Value);
+        //                if (response_.Content != null && response_.Content.Headers != null)
+        //                {
+        //                    foreach (var item_ in response_.Content.Headers)
+        //                        headers_[item_.Key] = item_.Value;
+        //                }
+
+        //                ProcessResponse(client_, response_);
+
+        //                var status_ = ((int)response_.StatusCode).ToString();
+        //                if (status_ == "200")
+        //                {
+        //                    var responseData_ = response_.Content == null ? null : await response_.Content.ReadAsStringAsync().ConfigureAwait(false);
+        //                    try
+        //                    {
+        //                        var result_ = Newtonsoft.Json.JsonConvert.DeserializeObject<FarmStatusResult[]>(responseData_);
+        //                        return result_;
+        //                    }
+        //                    catch (System.Exception exception_)
+        //                    {
+        //                        throw new DjustConnectException("Could not deserialize the response body.", (int)response_.StatusCode, responseData_, headers_, exception_);
+        //                    }
+        //                }
+        //                else if (status_ != "200" && status_ != "204")
+        //                {
+        //                    var responseData_ = response_.Content == null ? null : await response_.Content.ReadAsStringAsync().ConfigureAwait(false);
+        //                    throw new DjustConnectException("The HTTP status code of the response was not expected (" + (int)response_.StatusCode + ").", (int)response_.StatusCode, responseData_, headers_, null);
+        //                }
+        //                return null;
+        //            }
+        //            finally
+        //            {
+        //                if (response_ != null)
+        //                    response_.Dispose();
+        //            }
+        //        }
+        //    }
+        //    finally
+        //    {
+        //    }
+        //}
+
+        private string ConvertToString(object value, System.Globalization.CultureInfo cultureInfo)
+        {
+            if (value is Enum)
+            {
+                string name = Enum.GetName(value.GetType(), value);
+                if (name != null)
+                {
+                    var field = System.Reflection.IntrospectionExtensions.GetTypeInfo(value.GetType()).GetDeclaredField(name);
+                    if (field != null)
+                    {
+                        var attribute = System.Reflection.CustomAttributeExtensions.GetCustomAttribute(field, typeof(System.Runtime.Serialization.EnumMemberAttribute))
+                            as System.Runtime.Serialization.EnumMemberAttribute;
+                        if (attribute != null)
+                        {
+                            return attribute.Value;
+                        }
+                    }
+                }
+            }
+            else if (value is bool)
+            {
+                return Convert.ToString(value, cultureInfo).ToLowerInvariant();
+            }
+            else if (value is byte[])
+            {
+                return Convert.ToBase64String((byte[])value);
+            }
+            else if (value != null && value.GetType().IsArray)
+            {
+                var array = Enumerable.OfType<object>((Array)value);
+                return string.Join(",", Enumerable.Select(array, o => ConvertToString(o, cultureInfo)));
+            }
+
+            return Convert.ToString(value, cultureInfo);
+        }
+    }
 }
