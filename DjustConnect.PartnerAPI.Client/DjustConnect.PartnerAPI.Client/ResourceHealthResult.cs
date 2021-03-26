2eb6133a570b9fa3847224666bcf007728185469
Yannick Sagaert <yannick.sagaert@student.howest.be>
2021-03-26 14:17:32 +0100

    Update ResourceHealthResult

--- DjustConnect.PartnerAPI.Client/DjustConnect.PartnerAPI.Client/ResourceHealthResult.cs
+++ DjustConnect.PartnerAPI.Client/DjustConnect.PartnerAPI.Client/ResourceHealthResult.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,26 @@ namespace DjustConnect.PartnerAPI.Client
 {
     public class ResourceHealthResult
     {
+        [JsonProperty("resourceName", Required = Required.Always)]
+        public string ResourceName { get; set; }
+
+        [JsonProperty("lastResponse", Required = Required.Always)]
+        public string LastResponse { get; set; }
+
+        [JsonProperty("lastOkResponse", Required = Required.Always)]
+        public string LastOkResponse { get; set; }
+
+        [JsonProperty("resourceHealth", Required = Required.Always)]
+        public string ResourceHealth { get; set; } // string? result = "Unknown"
+
+        public string ToJson()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+
+        public static ResourceHealthResult FromJson(string data)
+        {
+            return JsonConvert.DeserializeObject<ResourceHealthResult>(data);
+        }
     }
 }
