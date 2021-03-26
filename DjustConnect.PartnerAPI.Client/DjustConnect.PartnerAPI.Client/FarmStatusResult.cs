fb1b92629a357f404a585a5d856a5cbf37a215d5
Yannick Sagaert <yannick.sagaert@student.howest.be>
2021-03-26 09:21:55 +0100

    Add FarmStatusResult class

---
+++ DjustConnect.PartnerAPI.Client/DjustConnect.PartnerAPI.Client/FarmStatusResult.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DjustConnect.PartnerAPI.Client
+{
+    public class FarmStatusResult
+    {
+        [JsonProperty("farmNumber", Required = Newtonsoft.Json.Required.Always)]
+        public string FarmNumber { get; set; }
+        [JsonProperty("status", Required = Newtonsoft.Json.Required.Always)]
+        public string Status { get; set; }
+
+        public string ToJson()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+
+        public static FarmStatusResult FromJson(string data)
+        {
+            return JsonConvert.DeserializeObject<FarmStatusResult>(data);
+        }
+    }
+}
