1818604bc28757b12430169f9a768f651a83ebfd
Yannick Sagaert <yannick.sagaert@student.howest.be>
2021-04-29 14:39:54 +0200

    Update DjustConnectClient with abstractions

--- DjustConnect.PartnerAPI.Client/DjustConnect.PartnerAPI.Client/DjustConnectClient.cs
+++ DjustConnect.PartnerAPI.Client/DjustConnect.PartnerAPI.Client/DjustConnectClient.cs
@@ -6,6 +6,7 @@ using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace DjustConnect.PartnerAPI.Client
@@ -27,7 +28,7 @@ namespace DjustConnect.PartnerAPI.Client
         {
             var store = new X509Store("My", StoreLocation.LocalMachine);
             store.Open(OpenFlags.ReadOnly);
-            var certificates = store.Certificates.Find(X509FindType.FindByThumbprint, thumbprint, false); 
+            var certificates = store.Certificates.Find(X509FindType.FindByThumbprint, thumbprint, false);
             if (certificates.Count == 0)
             {
                 throw new InvalidOperationException($"Certificate not found for CN=consumercsharp in LocalMachine/My.");
@@ -81,5 +82,112 @@ namespace DjustConnect.PartnerAPI.Client
             return request;
         }
 
+        protected async Task<PagedResult<T>> CallAPI<T>(StringBuilder urlBuilder, Func<string, T[]> getResult, CancellationToken cancellationToken)
+        {
+            var client_ = _httpClient;
+            try
+            {
+                using (var request_ = GetRequestMessage(urlBuilder))
+                {
+                    var response_ = await client_.SendAsync(request_, HttpCompletionOption.ResponseHeadersRead, cancellationToken).ConfigureAwait(false);
+                    try
+                    {
+                        var headers_ = response_.GetResponseHeaders();
+                        // TODO var status = response_.GetStatusCode();
+                        //var headers_ = GetResponseHeaders(response_);
+
+                        var status_ = ((int)response_.StatusCode).ToString();
+                        if (status_ == "200")
+                        {
+                            var responseData_ = response_.Content == null ? null : await response_.Content.ReadAsStringAsync().ConfigureAwait(false);
+                            try
+                            {
+                                var result_ = GetPagedResult<T>(headers_, getResult(responseData_));
+                                return result_;
+                            }
+                            catch (Exception exception_)
+                            {
+                                throw new DjustConnectException("Could not deserialize the response body.", (int)response_.StatusCode, responseData_, headers_, exception_);
+                            }
+                        }
+                        else if (status_ != "200" && status_ != "204")
+                        {
+                            var responseData_ = response_.Content == null ? null : await response_.Content.ReadAsStringAsync().ConfigureAwait(false);
+                            throw new DjustConnectException("The HTTP status code of the response was not expected (" + (int)response_.StatusCode + ").", (int)response_.StatusCode, responseData_, headers_, null);
+                        }
+                        return null;
+                    }
+                    finally
+                    {
+                        if (response_ != null)
+                            response_.Dispose();
+                    }
+                }
+            }
+            finally
+            {
+            }
+        }
+
+        protected async Task<T> CallAPI2<T>(StringBuilder urlBuilder, Func<Dictionary<string, IEnumerable<string>>, string, T> getResult, CancellationToken cancellationToken)
+        {
+            var client_ = _httpClient;
+            try
+            {
+                using (var request_ = GetRequestMessage(urlBuilder))
+                {
+                    var response_ = await client_.SendAsync(request_, HttpCompletionOption.ResponseHeadersRead, cancellationToken).ConfigureAwait(false);
+                    try
+                    {
+                        var headers_ = response_.GetResponseHeaders();
+                        // TODO var status = response_.GetStatusCode();
+                        //var headers_ = GetResponseHeaders(response_);
+
+                        var status_ = ((int)response_.StatusCode).ToString();
+                        if (status_ == "200")
+                        {
+                            var responseData_ = response_.Content == null ? null : await response_.Content.ReadAsStringAsync().ConfigureAwait(false);
+                            try
+                            {
+                                var result_ = getResult(headers_, responseData_);
+                                return result_;
+                            }
+                            catch (Exception exception_)
+                            {
+                                throw new DjustConnectException("Could not deserialize the response body.", (int)response_.StatusCode, responseData_, headers_, exception_);
+                            }
+                        }
+                        else if (status_ != "200" && status_ != "204")
+                        {
+                            var responseData_ = response_.Content == null ? null : await response_.Content.ReadAsStringAsync().ConfigureAwait(false);
+                            throw new DjustConnectException("The HTTP status code of the response was not expected (" + (int)response_.StatusCode + ").", (int)response_.StatusCode, responseData_, headers_, null);
+                        }
+                        return null;
+                    }
+                    finally
+                    {
+                        if (response_ != null)
+                            response_.Dispose();
+                    }
+                }
+            }
+            finally
+            {
+            }
+        }
+    }
+
+    internal static class Extensions
+    {
+        public static Dictionary<string, IEnumerable<string>> GetResponseHeaders(this HttpResponseMessage response_)
+        {
+            var headers_ = Enumerable.ToDictionary(response_.Headers, h_ => h_.Key, h_ => h_.Value);
+            if (response_.Content != null && response_.Content.Headers != null)
+            {
+                foreach (var item_ in response_.Content.Headers)
+                    headers_[item_.Key] = item_.Value;
+            }
+            return headers_;
+        }
     }
 }
