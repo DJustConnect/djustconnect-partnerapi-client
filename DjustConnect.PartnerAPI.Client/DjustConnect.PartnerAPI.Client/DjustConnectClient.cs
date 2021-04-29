e74b90358fd849683deca77ee21541e49ce15ec0
Yannick Sagaert <yannick.sagaert@student.howest.be>
2021-04-29 14:34:52 +0200

    Fix error with using statement

--- DjustConnect.PartnerAPI.Client/DjustConnect.PartnerAPI.Client/DjustConnectClient.cs
+++ DjustConnect.PartnerAPI.Client/DjustConnect.PartnerAPI.Client/DjustConnectClient.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
